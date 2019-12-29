2768b2a7a458478b3b33e9dc4b30b74441920680
nelson-caberto <nelson-caberto@users.noreply.github.com>
2019-12-28 19:31:24 -0600

    added csv output

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import json
+import csv
 
 with open('json.txt','r') as jsonFile:
     rgGames = json.load(jsonFile)
+
+keys = []
+for game in rgGames:
+    for key in game:
+        if key not in keys:
+            keys.append(key)
+
+with open('games.csv','w',newline='') as csvFile:
+    writer = csv.DictWriter(csvFile, fieldnames=keys)
+    writer.writeheader()
+    for game in rgGames:
+        writer.writerow(game)
